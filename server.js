9dd4e72ba4814717a12c50921aee833e9ad3a6e3
Jay Prakash <jayp7808@gmail.com>
2024-10-27 17:23:15 +0530

    change in cors

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connection.on("disconnected", () => {
 
 // middleWare
 app.use(cors({
-    origin: 'event-management-system-frontend-liart.vercel.app', // Allow requests from this origin
+    origin: 'https://event-management-system-frontend-liart.vercel.app', // Allow requests from this origin
     methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
     credentials: true, // Allow credentials (e.g., cookies)
 }));
