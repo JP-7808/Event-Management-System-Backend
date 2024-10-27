bcda23b9e89c256510f30ebb470c36ed80610f60
Jay Prakash <jayp7808@gmail.com>
2024-10-27 20:04:41 +0530

    chnage in verifyToken

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token; // Ensure the cookie is being sent in the request
+    const token = req.cookies.access_token || req.headers.authorization?.split(' ')[1]; // Ensure the cookie is being sent in the request
 
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied check' });
