242fb8328cb375f0f40a6cfffeb457e3a30d2f3d
Jay Prakash <jayp7808@gmail.com>
2024-11-01 21:51:06 +0530

    reveser

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,19 +3,19 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token || req.headers.authorization?.split(' ')[1];
+    const token = req.cookies.access_token || req.headers.authorization?.split(' ')[1]; 
     console.log("check token", token);
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied check' });
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => { 
         if (error) {
-            console.log("Token verification error:", error.message);
+            console.log("Token verification error:", error.message); 
             return res.status(401).json({ msg: 'Token is not valid' });
         }
 
-        req.user = user;
+        req.user = user; 
         next();
     });
 };
